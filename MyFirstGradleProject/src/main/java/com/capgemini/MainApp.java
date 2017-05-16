472b0d9ffd59172880b36f0c6c3813161652fe9a
AkbarSaleem <akbar.babu@capgemini.com>
2017-05-16 15:13:26 +0530

    Update MainApp.java

--- MyFirstGradleProject/src/main/java/com/capgemini/MainApp.java
+++ MyFirstGradleProject/src/main/java/com/capgemini/MainApp.java
@@ -4,6 +4,7 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world from gradle");
+		System.out.println("Chnaging the content in git hub");
 
 	}
 
